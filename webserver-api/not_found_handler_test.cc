3c4ae802afb330b3a2278d1850e0dc80654bed04
Evelyn <chenyiying@ucla.edu>
2017-02-22 13:01:48 -0800

    edited not_found_handler_test.cc

--- webserver-api/not_found_handler_test.cc
+++ webserver-api/not_found_handler_test.cc
@@ -2,6 +2,7 @@
 #include "not_found_handler.h"
 #include "request.h"
 #include "response.h"
+#include "file_handler.h"
 
 #include <string>
 
@@ -10,11 +11,11 @@ TEST(NotFoundHandlerTest, SimpleTest)
 	std::string raw_request = "";
 	auto req = Request::Parse(raw_request.c_str());
 
-	FileHandler file_handler("static/");
+	FileHandler file_handler("./test_folder");
 	Response resp;
 	file_handler.HandleRequest(*req, &resp);
 	std::string resp_str = resp.ToString();
 	
-	int RN = resp.find("\r\n");
-	EXPECT_EQ(resp_str.substr(0,RN), "HTTP/1.1 404 Not Found");
+	int RN = resp_str.find("\r\n");
+	EXPECT_EQ(resp_str.substr(0,RN), "HTTP/1.0 404 Not Found");
 }
